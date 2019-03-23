27a2f0460873a3bf0a223e969e848269738b6e7a
Elizabeth <efkarst@outlook.com>
2019-03-22 17:02:38 -0700

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,35 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// UUDDLRLRba
+let index = 0
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  document.addEventListener('keydown', function(e) {return onKeyDownHandler(e)})
 }
+
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+
+  if (key === code[index]) {
+    index++;
+    if (index === code.length) {
+      alert("Hurray!");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+
+
+// const main = document.getElementById('main');
+ 
+// main.addEventListener('click', function(event) {
+//   alert('I was clicked!');
+// });
+
+
+// const input = document.querySelector('input');
+ 
+// input.addEventListener('keydown', function(e) {
+//   console.log(e.which);
+// });
